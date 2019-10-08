5c69cfb4d83716629f0c484049c00ea6b6ead14d
Darren Powell <darrenpowell@Darrens-MBP.lan>
2019-10-08 09:58:35 +0200

    working email application

---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,13 @@
+package emailapp;
+
+import java.util.Scanner;
+
+public class EmailApp {
+    public static void main(String[] args) {
+
+        Email em1 = new Email("Darren", "Powell");
+
+        System.out.println(em1.showInfo());
+    }
+
+}
